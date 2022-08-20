6692e86b1921a258008b25618fb7dd4da86e5dee
hargun0360 <hargunsingh0720@gmail.com>
2022-08-21 01:55:25 +0530

    Auth Integration Done

---
+++ src/redux/actions/AuthAction.js
@@ -0,0 +1,36 @@
+export  const userEmail = (data)=>{
+    return{
+        type:"User_Email",
+        payload:data
+    }
+}
+export  const userPass = (data)=>{
+    return{
+        type:"User_Pass",
+        payload:data
+    }
+}
+export  const userName = (data)=>{
+    return{
+        type:"User_Name",
+        payload:data
+    }
+}
+export  const userMobile = (data)=>{
+    return{
+        type:"User_Mobile",
+        payload:data
+    }
+}
+export  const userGender = (data)=>{
+    return{
+        type:"User_Gender",
+        payload:data
+    }
+}
+export  const userType = (data)=>{
+    return{
+        type:"User_Type",
+        payload:data
+    }
+}
