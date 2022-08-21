631a5004cb464b4db878f5507e01d38a0f20f18b
saksham-tiwari <sakshamt234@gmail.com>
2022-08-21 05:37:38 +0530

    change basename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <App />
     </Router>
   </Provider>
